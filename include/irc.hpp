4836eba9101979f3c799df6bfe4d8e9a3db2d9ac
Zakaria Farah <zakaria.farah55@outlook.com>
2026-03-17 20:58:21 +0200

    wip

--- include/irc.hpp
+++ include/irc.hpp
@@ -3,5 +3,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+// io libs
+#include <iostream>
 
 using SA = struct sockaddr_in;
