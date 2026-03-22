7425cc4471f3de8e24f1d9f3a0172c7c8520eff6
jackie jan <zakaria.farah55@outlook.com>
2026-03-22 21:54:24 +0200

    moved under utils

---
+++ src/utils/Logger.cpp
@@ -0,0 +1,54 @@
+#include "Logger.hpp"
+#include <iostream>
+
+void Logger::log(const std::string &msg, const Level &level)
+{
+    if (level < this->level)
+        return;
+    std::cout << "[" << getLevel(level) << "] - " << msg << std::endl;
+}
+
+void Logger::info(const std::string &msg)
+{
+     Logger *logger = Logger::getLogger();
+     logger->log(msg, INFO);
+}
+
+void Logger::warning(const std::string &msg)
+{
+     Logger *logger = Logger::getLogger();
+     logger->log(msg, WARN);
+}
+
+void Logger::error(const std::string &msg)
+{
+     Logger *logger = Logger::getLogger();
+     logger->log(msg, ERROR);
+}
+
+void Logger::debug(const std::string &msg)
+{
+    Logger *logger = Logger::getLogger();
+    logger->log(msg, DEBUG);
+}
+
+ void Logger::setLevel(const Level &level)
+{
+    this->level = level;
+}
+
+std::string Logger::getLevel(void)
+{
+     return this->levels[level];
+}
+
+std::string Logger::getLevel(const Level &level)
+{
+    return this->levels[level];
+}
+
+ Logger *Logger::getLogger(void)
+{
+    static Logger log = Logger();
+    return &log;
+}
