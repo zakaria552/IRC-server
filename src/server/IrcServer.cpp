f7ab9652f146d130cf82f2af2fce164bfe3ceb83
jackie jan <zakaria.farah55@outlook.com>
2026-03-22 21:54:16 +0200

    Able to handle multiple clients in a non-blocking fashion

---
+++ src/server/IrcServer.cpp
@@ -0,0 +1,105 @@
+#include "IrcServer.hpp"
+#include "Logger.hpp"
+
+IrcServer::IrcServer(const char *port, const char *password) : port(std::atoi(port)), password(password)
+{
+    struct addrinfo req{}, *res, *p;
+    req.ai_family = AF_UNSPEC;
+    req.ai_flags = AI_PASSIVE;
+    if (getaddrinfo(nullptr, port, &req, &res) != 0)
+    {
+        Logger::error("Failed to retrieve host address information");
+        std::runtime_error("Failed to retrieve host address information");
+    }
+    for (p = res; p != nullptr; p = p->ai_next)
+    {
+        if (p->ai_protocol == AF_ROUTE)
+            continue;
+        if ((socketFd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) != -1)
+        {
+            Logger::info("AF: "+ std::to_string(p->ai_family) +
+                        ", sockType: " + std::to_string(p->ai_socktype) +
+                        ", AF_PROTOCAL: " + std::to_string(p->ai_protocol));
+            break;
+        }
+    }
+    if (socketFd < 0)
+    {
+        Logger::error("Failed to create socket");
+        std::runtime_error("Failed to create socket");
+    }
+    if (bind(socketFd, p->ai_addr, p->ai_addrlen) != 0)
+    {
+        freeaddrinfo(res);
+        Logger::error("Failed binding socket to address");
+        std::runtime_error("Failed bindng socket to address");
+    }
+    freeaddrinfo(res);
+    pollfd newPoll = {};
+    newPoll.fd = socketFd;
+    newPoll.events = POLLIN;
+    ioEvents.add(newPoll);
+}
+
+void IrcServer::start()
+{
+    if (listen(socketFd, DEFAULT_BACKLOG) != 0)
+    {
+        Logger::error("Failed listening for connections on the socket" + std::string(strerror(errno)));
+        return;
+    }
+    while (!closeConnection)
+    {
+        ioEvents.pollEvents();
+        int i = 0;
+        for (auto client: ioEvents)
+        {
+            Logger::info(std::to_string(client.fd));
+            if (!(client.revents & POLLIN) && ++i)
+                continue;
+            if (client.fd == socketFd && ++i)
+            {
+                newClient();
+                continue;
+            }
+            char buff[1024];
+            int n = read(client.fd, buff, 1024);
+            if (n > 0)
+                processRequest(client.fd, buff, n);
+            else if (n == 0)
+                clientDisconnected(i);
+            else
+                Logger::warning("Read failed");
+            i++;
+        }
+    }
+}
+
+void IrcServer::newClient()
+{
+    Logger::info("new connection");
+    struct sockaddr_storage clientAddr;
+    pollfd newPoll = {};
+    socklen_t addrLen;
+    int clientFd;
+    if ((clientFd = accept(socketFd, (struct sockaddr *)&clientAddr, &addrLen)) < 0)
+    {
+        Logger::warning("Failed to accept client");
+        return;
+    }
+    newPoll.fd = clientFd;
+    newPoll.events = POLLIN;
+    ioEvents.add(newPoll);
+}
+
+void IrcServer::processRequest(const int clientFd, const char *body, const size_t length)
+{
+    Logger::info("Processing client request");
+    send(clientFd, body, length, MSG_DONTWAIT | MSG_NOSIGNAL);
+}
+
+void IrcServer::clientDisconnected(const int index)
+{
+    Logger::info("Client disconnected");
+    ioEvents.remove(index);
+}
---
+++ src/server/IrcServer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <netdb.h>
+#include <string>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include "IOEventPoller.hpp"
+#include <unistd.h>
+
+#define DEFAULT_BACKLOG 10
+class IrcServer
+{
+    int port;
+    int socketFd = -1;
+    std::string password;
+    bool closeConnection = false;
+    IOEventPoller ioEvents;
+public:
+    IrcServer() = delete;
+    IrcServer(const char *port, const char *password);
+    void start();
+    void newClient();
+    void clientDisconnected(const int index);
+    void processRequest(const int clientFd, const char *body, const size_t length);
+};
