c4122382a99c5c5809f4e40bb33e445501a30732
Zakaria Farah <zakaria.farah55@outlook.com>
2026-04-17 15:44:04 +0300

    add: Timestamp to logs

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -1,11 +1,15 @@
 #include "Logger.hpp"
 #include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 void Logger::log(const std::string &msg, const Level &level)
 {
     if (level < this->level)
         return;
-    std::cout << "[" << getLevel(level) << "] - " << msg << std::endl;
+    std::cout << "[" << timestamp() << "][" << getLevel(level) << "] - " << msg << std::endl;
 }
 
 void Logger::info(const std::string &msg)
@@ -51,4 +55,14 @@ std::string Logger::getLevel(const Level &level)
 {
     static Logger log = Logger();
     return &log;
+}
+
+std::string Logger::timestamp()
+{
+    const auto now = std::chrono::system_clock::now();
+    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::tm tm = *std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
 }
