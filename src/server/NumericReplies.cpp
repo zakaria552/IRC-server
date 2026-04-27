d950fd09c3cd0683de769dcdb5a48476b6e1ff40
Zakaria Farah <zakaria.farah55@outlook.com>
2026-04-27 16:56:45 +0300

    refactor: cleaner abstraction, using only static function

---
+++ src/server/NumericReplies.cpp
@@ -0,0 +1,42 @@
+#include "NumericReplies.hpp"
+#include "server/Client.hpp"
+#include <string>
+#include <sys/socket.h>
+#include "server/globals.hpp"
+
+std::string NumericReplies::makeBody(int errCode, const std::string &nick, const std::string &channel, const std::string &msg)
+{
+    std::string body = nick + " #" + channel + " :" + msg;
+    std::string fullBody = ":" + serverName + " " + std::to_string(errCode) + " " + body + "\r\n";
+    return fullBody;
+}
+
+Message NumericReplies::channelNotFound(const std::string &channel, const Client &client)
+{
+    return {client.getSocket(), NumericReplies::makeBody(402, client.getNick(), channel, "No such channel")};
+}
+
+Message NumericReplies::notChannelMember(const std::string &channel, const Client &client)
+{
+    return {client.getSocket(), NumericReplies::makeBody(442, client.getNick(), channel, "You're not on that channel")};
+}
+
+Message NumericReplies::isChannelMember(const std::string &channel, const Client &sender, const Client &receiver)
+{
+    return {sender.getSocket(), NumericReplies::makeBody(443, sender.getNick() + " " + receiver.getNick(), channel, "is already on channel")};
+}
+
+Message NumericReplies::isInviteOnly(const std::string &channel, const Client &client)
+{
+    return {client.getSocket(), NumericReplies::makeBody(473, client.getNick(), channel, "Cannot join channel (+i)")};
+}
+
+
+std::string NumericReplies::welcome()
+{
+    return "001 ";
+};
+
+std::string NumericReplies::passMisMatch() {
+    return "464 : Invalid password\r\n";
+};
---
+++ src/server/NumericReplies.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "server/Client.hpp"
+#include <string>
+#include "server/QueueMessages.hpp"
+
+class NumericReplies
+{
+public:
+    NumericReplies() = delete;
+    static std::string makeBody(int errCode, const std::string &nick, const std::string &channel, const std::string &msg);
+    static std::string welcome();
+    static std::string passMisMatch();
+    static Message channelNotFound(const std::string &channel, const Client &client);
+    static Message notChannelMember(const std::string &channel, const Client &client);
+    static Message isChannelMember(const std::string &channel, const Client &sender, const Client &receiver);
+    static Message isInviteOnly(const std::string &channel, const Client &client);
+    static Message setServerName(const std::string &name);
+};
