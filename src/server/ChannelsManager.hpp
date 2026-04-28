fb9b7ae3cca7e6a695bc4c84ec2414535c0a0827
Zakaria Farah <zakaria.farah55@outlook.com>
2026-04-28 16:23:55 +0300

    add: func that broadcasts modes

--- src/server/ChannelsManager.hpp
+++ src/server/ChannelsManager.hpp
@@ -15,6 +15,7 @@ public:
     ChannelsManager(std::queue<BroadcastMessage> &queues);
     void add(const std::string &channel, int clientFd);
     void sendMessage(const Client &sender , const std::string &targets, const std::string &msg);
+    void broadcastModeChange(const Client &client, const std::string &channel, const std::string &rawCmd);
     bool channelExist(const std::string &channel);
     bool isMemberOfChannel(const std::string &channel, int clientFd);
     void updateChannelMode(const std::string &channel, Mode mode, char intent);
