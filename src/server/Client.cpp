e50256fdafc585b3062241f0a24f2e7bd2b8efc4
Zakaria Farah <zakaria.farah55@outlook.com>
2026-05-06 17:44:43 +0300

    add: client stores its ip address

--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -1,18 +1,61 @@
 #include "Client.hpp"
 
-Client::Client(int socket): socket(socket){};
+Client::Client(int socket, const std::string &ipAddress): socket(socket), ip(ipAddress)
+{
+};
 
-const std::string &Client::getNick() const {return nickname;}
-const std::string &Client::getPass() const {return password;};
-const std::string &Client::getUsername() const {return username;};
-const std::string &Client::getFullname() const {return fullName;};
-int Client::getSocket() const {return socket;};
+const std::string &Client::getNick() const
+{
+    return nickname;
+}
 
-void Client::setNick(const std::string &nick) {nickname = nick;}
-void Client::setPass(const std::string &pass) {password = pass;};
-void Client::setUsername(const std::string &user) {username = user;};
-void Client::setFullname(const std::string &name) {fullName = name;};
+const std::string &Client::getPass() const
+{
+    return password;
+};
 
-bool Client::isOperator() {return operatorPriv;};
-void Client::promote() {operatorPriv = true;};
-void Client::demote() {operatorPriv = false;};
+const std::string &Client::getUsername() const
+{
+    return username;
+};
+
+const std::string &Client::getFullname() const
+{
+    return fullName;
+};
+
+int Client::getSocket() const
+{
+    return socket;
+};
+
+const std::string &Client::getIpAddress() const
+{
+    return ip;
+}
+
+void Client::setNick(const std::string &nick)
+{
+    nickname = nick;
+}
+
+void Client::setPass(const std::string &pass)
+{
+    password = pass;
+}
+
+void Client::setUsername(const std::string &user)
+{
+    username = user;
+}
+
+void Client::setFullname(const std::string &name)
+{
+    fullName = name;
+}
+
+
+std::string Client::info() const
+{
+    return std::to_string(socket) + "," + nickname + "," + ip;
+}
--- src/server/Client.hpp
+++ src/server/Client.hpp
@@ -4,6 +4,7 @@
 class Client
 {
     int socket;
+    std::string ip;
     std::string password;
     std::string nickname;
     std::string username;
@@ -11,19 +12,17 @@ class Client
     bool operatorPriv = false;
 public:
     Client() =  default;
-    Client(int socket);
+    Client(int socket, const std::string &ipAddress);
     ~Client() = default;
     const std::string &getNick() const;
     const std::string &getPass() const;
     const std::string &getUsername() const;
     const std::string &getFullname() const;
     int getSocket() const;
+    const std::string &getIpAddress() const;
     void setNick(const std::string &nick);
     void setPass(const std::string &pass);
     void setUsername(const std::string &username);
     void setFullname(const std::string &fullname);
-
-    bool isOperator();
-    void promote();
-    void demote();
+    std::string info() const;
 };
